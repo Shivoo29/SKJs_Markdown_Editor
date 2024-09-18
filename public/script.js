d2778774e0a2ff68bcbf373718189ac00e734dc3
Shivoo29 <2004skj@gmail.com>
2024-09-18 22:19:18 +0530

    dill mera jese koi dish antina tha

---
+++ public/script.js
@@ -0,0 +1,5 @@
+Window.onload = function{
+  var converter = new Showdown.converter();
+  var pad = document.getElementById('pad');
+  var markdownArea = document.getElementById('markdownArea');
+};
