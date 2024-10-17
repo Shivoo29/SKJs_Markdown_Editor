937c418ec6f49dc8805ab98533522da5d5fed0a0
Shivoo29 <2004skj@gmail.com>
2024-10-17 15:16:21 +0530

    tu hi jeene ki wahja h tu hi marne ka sabak h

--- public/script.js
+++ public/script.js
@@ -1,5 +1,15 @@
-Window.onload = function{
-  var converter = new Showdown.converter();
+window.onload = function() {
+  var converter = new showdown.Converter();
   var pad = document.getElementById('pad');
-  var markdownArea = document.getElementById('markdownArea');
+  var markdownArea = document.getElementById('markdown');
+
+  var convertTextAreaToMarkdown = function(){
+      var markdownText = pad.value;
+      html = converter.makeHtml(markdownText);
+      markdownArea.innerHTML = html;
+  };
+
+  pad.addEventListener('input', convertTextAreaToMarkdown);
+
+  convertTextAreaToMarkdown();
 };
